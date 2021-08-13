88571ae4596cb925446b0eb910495163e3894d29
paul77uk <paul77uk@gmail.com>
2021-08-13 16:13:46 +0100

    11th commit - RemindersListFragmentTest error still

--- app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -166,7 +166,7 @@ class ReminderListFragmentTest : AutoCloseKoinTest() {
 
     @Test
     fun deleteAllReminders_displayNoDataTextView() = runBlockingTest {
-
+//
         val remindersDao: RemindersDao = get()
 
         val reminder = ReminderDTO(
